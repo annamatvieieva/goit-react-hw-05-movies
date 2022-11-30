3c7338fe80b96bb0ed2c0c8dc528e4e7ea1f8950
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-11-30 17:58:23 +0200

    add route path *

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import ShareLayout from './ShareLayout/ShareLayout';
 
@@ -19,6 +19,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<Navigate to="/" replace/>} />
         </Route>
       </Routes>
     </div>
